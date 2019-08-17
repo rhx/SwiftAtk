d79d6c8d84305e56a458f1fd34dcd371c30e4f3a
Rene Hexel <r.hexel@griffith.edu.au>
2019-08-17 10:57:46 +1000

    Integrate CAtk as a system library

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
 import PackageDescription
 
@@ -8,11 +8,15 @@ let package = Package(
         .library(name: "Atk", targets: ["Atk"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/rhx/CAtk.git", .branch("master")),
         .package(url: "https://github.com/rhx/SwiftGObject.git", .branch("master"))
     ],
     targets: [
-        .target(name: "Atk", dependencies: ["GLibObject"]),
+	.systemLibrary(name: "CAtk", pkgConfig: "atk glib-2.0 gio-unix-2.0",
+	    providers: [
+		.brew(["atk", "glib", "glib-networking", "gobject-introspection"]),
+		.apt(["libatk1.0-dev", "libglib2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
+	    ]),
+        .target(name: "Atk", dependencies: ["CAtk", "GLibObject"]),
         .testTarget(name: "AtkTests", dependencies: ["Atk"]),
     ]
 )
