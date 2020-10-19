4f9f95d9bbfa24f8d685e1c6ba0c9db718e3f81e
Mikoláš Stuchlík <stuchlej@gmail.com>
2020-10-20 01:30:43 +0200

    Remove need for build sh

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ let package = Package(
     name: "Atk",
     products: [ .library(name: "Atk", targets: ["Atk"]) ],
     dependencies: [
-        .package(name: "GLibObject", url: "https://github.com/rhx/SwiftGObject.git", .branch("master"))
+        .package(name: "GLibObject", url: "https://github.com/mikolasstuchlik/SwiftGObject.git", .branch("master"))
     ],
     targets: [
-	.systemLibrary(name: "CAtk", pkgConfig: "atk glib-2.0 gio-unix-2.0",
+	.systemLibrary(name: "CAtk", pkgConfig: "atk",
 	    providers: [
 		.brew(["atk", "glib", "glib-networking", "gobject-introspection"]),
 		.apt(["libatk1.0-dev", "libglib2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
