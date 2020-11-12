2d0946c74014b6219989ce75b2869415a86f125f
Mikoláš Stuchlík <stuchlej@gmail.com>
2020-11-12 17:58:16 +0100

    Add and remove files for building queue

--- Package.swift
+++ Package.swift
@@ -8,8 +8,6 @@ let package = Package(
     dependencies: [
         .package(name: "gir2swift", url: "https://github.com/mikolasstuchlik/gir2swift.git", .branch("master")),
         .package(name: "GLibObject", url: "https://github.com/mikolasstuchlik/SwiftGObject.git", .branch("master")),
-        //.package(name: "GIO", url: "https://github.com/mikolasstuchlik/SwiftGIO.git", .branch("master")),
-        //.package(name: "GModule", url: "https://github.com/mikolasstuchlik/SwiftGModule.git", .branch("master")),
     ],
     targets: [
 	.systemLibrary(name: "CAtk", pkgConfig: "atk",
