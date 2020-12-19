1769c23c7d7b254fadc7ed85dc9d83dcd1f698e1
Mikoláš Stuchlík <stuchlej@gmail.com>
2020-12-19 16:18:40 +0100

    Attempt to solve lldb issue by adding flags

--- Package.swift
+++ Package.swift
@@ -15,7 +15,11 @@ let package = Package(
 		.brew(["atk", "glib", "glib-networking", "gobject-introspection"]),
 		.apt(["libatk1.0-dev", "libglib2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
 	    ]),
-        .target(name: "Atk", dependencies: ["CAtk", "GLibObject"]),
+        .target(
+            name: "Atk", 
+            dependencies: ["CAtk", "GLibObject"],
+            swiftSettings: [.unsafeFlags(["-Xfrontend", "-serialize-debugging-options"], .when(configuration: .debug))]
+        ),
         .testTarget(name: "AtkTests", dependencies: ["Atk"]),
     ]
 )
